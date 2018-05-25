7347372c414be9f984b19eb4dc79456cd1c82e08
Serhii Tymoshenko <temosh@i.ua>
2018-05-25 20:18:50 +0300

    [DB-33] Database tables for storing events and requests

--- db/debook_db_data.sql
+++ db/debook_db_data.sql
@@ -22,3 +22,16 @@ INSERT INTO `debook_db`.`credit_type` (`credit_type_id`, `type`) VALUES (1, 'LOA
 INSERT INTO `debook_db`.`credit_type` (`credit_type_id`, `type`) VALUES (2, 'DEBT');
 
 COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `debook_db`.`event_type`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `debook_db`;
+INSERT INTO `debook_db`.`event_type` (`event_type_id`, `type`, `direction`) VALUES (1, 'CONNECTION_REQUEST', 'INCOMING');
+INSERT INTO `debook_db`.`event_type` (`event_type_id`, `type`, `direction`) VALUES (2, 'CONNECTION_REQUEST', 'OUTGOING');
+INSERT INTO `debook_db`.`event_type` (`event_type_id`, `type`, `direction`) VALUES (3, 'DEBT_REQUEST', 'INCOMING');
+INSERT INTO `debook_db`.`event_type` (`event_type_id`, `type`, `direction`) VALUES (4, 'DEBT_REQUEST', 'OUTGOING');
+
+COMMIT;
--- db/debook_db_test_data.sql
+++ db/debook_db_test_data.sql
@@ -1,12 +1,15 @@
 -- -----------------------------------------------------
 -- Data for table `debook_db`.`user`
+-- Password for test users - 'test'
 -- -----------------------------------------------------
--- START TRANSACTION;
--- USE `debook_db`;
--- INSERT INTO `user` VALUES (1, 'Temosh', 'Serhii', 'Tymoshenko', 'temosh@i.ua', '1');
--- INSERT INTO `user` VALUES (2, 'NickFury', 'Nick', 'Fury', 'n.fury@shield.com', '1');
---
--- COMMIT;
+START TRANSACTION;
+USE `debook_db`;
+INSERT INTO `user` VALUES (1, 'Test', 'First', 'Last', 'test@test.com', '1');
+INSERT INTO `user` VALUES (2, 'NickFury', 'Nick', 'Fury', 'n.fury@shield.com', '1');
+INSERT INTO `user_secret` VALUES (1, '$2a$10$3wuhm.O7XA1Cy/n76.Nw7OUuvhdQav0xVYpkgHmD05qYd7hJEQyiG');
+INSERT INTO `user_secret` VALUES (2, '$2a$10$3VTgoqfKqI/d9rSnysTxgOZPh7cklucLYq7YaQHJ77v5YsmPFLrtG');
+
+COMMIT;
 
 -- ----------------------------------------
 -- NOTE: 2 users should be addded manually!
